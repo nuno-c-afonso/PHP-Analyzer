3b01a8dd80a8e9b3c0594d14915ce29fbc1bc808
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-11-23 12:10:47 +0000

    Debugging HTML slice

--- Application.py
+++ Application.py
@@ -1,22 +1,28 @@
 from PHPparser import *
 from VulnerabilityPattern import *
+import os
 
-path = os.environ['HOME'] +  "/projSS/proj-slicesX/"
-#path = "/home/diogo/Desktop/projSS/proj-slicesX/"
+path = os.getcwd() + "/Slices/"
 print path
 
-files = ["sqli_01.txt","sqli_02.txt","sqli_03.txt","sqli_04.txt","sqli_05.txt","xss_01.txt","xss_02.txt","xss_03.txt"]#,
+#files = ["sqli_01.txt","sqli_02.txt","sqli_03.txt","sqli_04.txt","sqli_05.txt","xss_01.txt","xss_02.txt","xss_03.txt"]#,
         #"teste.txt" ]
-#files = ["sqli_04.txt"]
-#files = ["teste.txt"]
+files = ["xss_02.txt"]
+
+filename = raw_input("Please input the filename of the patterns' file (<enter> for the default option)\n> ").strip()
+filename = "PatternsFile.txt"
+patterns = patterns_from_file(filename)
+
 slices = []
 for file in files:
     print("\n" + "\n" + "#" * 63 + "\n" + "#" * 23 + file + "#" * 23 + "\n" + "#" * 63 + "\n")
-    slices.append(Slice(path + file))
+    for pattern in patterns:
+
+        #TODO: Remove after debugging
+        if pattern.vulnerabilityName == "Cross Site Scripting":
+            print("\n" + "\n" + "#" * 63 + "\n" + "#" * 23 + pattern.vulnerabilityName + "#" * 23 + "\n" + "#" * 63 + "\n")
+            slices.append(Slice(path + file, pattern))
 
 #for slice in slices:
 #    slice.process()
-
-
-
 # string = re.
--- PHPparser.py
+++ PHPparser.py
@@ -1,98 +1,16 @@
 import re
-import os
-from phply import *
-
-global SQLIsinks
-SQLIsinks=("mysql_query\((.*?)\)",
-             "mysql_unbuffered_query\((.*?)\)",
-             "mysql_db_query\((.*?)\)",
-             "mysqli_query\((.*?)\)",
-             "mysqli_real_query\((.*?)\)",
-             "mysqli_master_query\((.*?)\)",
-             "mysqli_multi_query\((.*?)\)",
-             "mysqli_stmt_execute\((.*?)\)",
-             "mysqli_execute\((.*?)\)",
-             "mysqli::query\((.*?)\)",
-             "mysqli::multi_query\((.*?)\)",
-             "mysqli::real_query\((.*?)\)",
-           "mysqli_stmt::execute\((.*?)\)",
-           "db2_exec\((.*?)\)",
-           "pg_query\((.*?)\)",
-           "pg_send_query\((.*?)\)")
-global XSSsinks
-XSSsinks = ("echo",
-            "print",
-            "printf",
-            "die",
-            "error",
-            "exit",
-            "file_put_contents",
-            "file_get_contents",)
-
-
-global SQLIentrys
-SQLIentrys = ("$_GET",
-            "$_POST",
-            "$_COOKIE",
-            "$_REQUEST",
-            "HTTP_GET_VARS",
-            "HTTP_POST_VARS",
-            "HTTP_COOKIE_VARS",
-            "HTTP_REQUEST_VARS",)
-
-
-global MySQLSanitization
-MySQLSanitization=("mysql_escape_string(%s)",
-                "mysql_real_escape_string(%s)",
-                "mysqli_escape_string(%s)",
-                "mysqli_real_escape_string(%s)",
-                "mysqli_stmt_bind_param(%s)",
-                "mysqli::escape_string(%s)",
-                "mysqli::real_escape_string(%s)",
-                "mysqli_stmt::bind_param(%s)",
-                "db2_escape_string(%s)",
-                "pg_escape_string(%s)",
-                "pg_escape_bytea(%s)",)
-
-
-
-
-
-globalxxx =(            "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "", "",
- "", "", "", "", "", "")
-
-
-
-
-
-
+from VulnerabilityPattern import *
 
 class Slice:
-    def __init__(self, filePath, vp = None):
+    def __init__(self, filePath, vp):
         self.vp = vp
-        if self.vp == None:
-            self.vp = VulnerabilityPattern("sqli",SQLIentrys,MySQLSanitization,XSSsinks+SQLIsinks)
 
         self.identation = 0
         content_file = open(filePath, 'r')
         content = content_file.read()
         print(" "*61 + "\n" + "-"*23 + " slice content " + "-"*23 + "\n" + " "*61 + "\n" + "\n" + content)
         lines = content.split('\n')
-        atributionPatern = re.compile("\$([a-z]\w*)\s*=\s*(.*?)$")
+        atributionPatern = re.compile("\$([a-zA-Z_]\w*)\s*=\s*(.*?)$")
         atribution_completed =[]
         self.slice_order =[]
         imcomplete = False
@@ -116,7 +34,7 @@ class Slice:
                     atribution_completed.append(line)
                     #slice_order.append(PHPatribution(line, self.identation+1))
 
-            elif line.startswith('<') & line.endswith('>;'):
+            elif line.startswith('<') & line.endswith('>'):
                 atribution_completed.append(line)
                 self.slice_order.append(HTMLline(line, self.identation+1,self.vp))
 
@@ -138,7 +56,7 @@ class Slice:
 
 
 def IsSink(line, vpatern):
-    for sinkType in vpatern.sensitive_sinks:
+    for sinkType in vpatern.sensitiveSinks:
         if re.search(sinkType, line) != None:
             return True
     return False
@@ -173,11 +91,18 @@ class HTMLline:
         self.identation = identation
         print(identation * "\t" + "HTMLline: " + string)
         self.vars = []
-        for sink in XSSsinks:
-            if re.search("(.*?<?php\s*" + sink + "\s*\$[a-z]\w*\s*.*?)", string) != None:
-                groups = re.findall("(.*?<?php\s*" + sink + "\s*\$[a-z]\w*\s*.*?)", string)
+        for sink in vpatern.sensitiveSinks:
+            """
+            if re.search("(.*?<\?php\s*" + sink + "\s*\$[a-z]\w*\s*.*?)", string) != None:
+                groups = re.findall("(.*?<\?php\s*" + sink + "\s*\$[a-z]\w*\s*.*?)", string)
                 for cut in groups:
-                    var = re.search(".*?<?php\s*" + sink + "\s*(\$[a-z]\w*)\s*.*?", cut).groups()
+                    var = re.search(".*?<\?php\s*" + sink + "\s*(\$[a-z]\w*)\s*.*?", cut).groups()
+                    self.vars.append(PHPvar(var[0], identation + 1, vpatern))
+            """
+            if re.search("(<\?php\s*" + sink + "\s*\(?.*\)?.*\?>)", string) != None:
+                groups = re.findall("(<\?php\s*" + sink + "\s*\(?.*\)?.*\?>)", string)
+                for cut in groups:
+                    var = re.search("(<\?php\s*" + sink + "\s*\(?.*\)?.*\?>)", cut).groups()
                     self.vars.append(PHPvar(var[0], identation + 1, vpatern))
 
         self.string = string;
@@ -223,7 +148,7 @@ class Sink:
             self.vars.append(PHPvar(cut, identation + 1,vpatern))
 
         #groups = re.findall("\s*(\$_[a-z]\w*)\s*", string)
-        for entryType in vpatern.entry_points:
+        for entryType in vpatern.entryPoints:
             groups = re.findall("\s*(\\"+ entryType +"\[['a-zA-Z0-9]*\])\s*", string)
             for cut in groups:
                 self.entries.append(PHPentry(cut, identation+1, vpatern))
@@ -234,10 +159,10 @@ class Sink:
         for var in self.vars:
             if vars.get(var.name) != None:
                 if vars.get(var.name) == "low":
-                    print("X-->"+vpatern.vulnerabillity_name+" in " + self.string + " because of " + var.name)
+                    print("X-->"+vpatern.vulnerabilityName+" in " + self.string + " because of " + var.name)
                     integrity = "low"
         for entry in self.entries:
-            print("X--> "+vpatern.vulnerabillity_name+" in " + self.string + " because of " + entry.string)
+            print("X--> "+vpatern.vulnerabilityName+" in " + self.string + " because of " + entry.string)
             integrity = "low"
         return integrity
 
@@ -293,31 +218,32 @@ class PHPvar:
                     return vars.get(self.name)
         return "low"
 
+class UnknownRValue:
+    def __init__(self, string, identation, vpatern):
+        print(identation * "\t" + "UnknownRValue: " + string)
+        self.name = string;
 
-class VulnerabilityPattern:
-        def __init__(self, vulnerabillity_name, entry_points, sanitization_functions, sensitive_sinks):
-            self.vulnerabillity_name = vulnerabillity_name;
-            self.entry_points = entry_points;
-            self.sanitization_functions = sanitization_functions;
-            self.sensitive_sinks = sensitive_sinks;
+    def process(self, vars,vpatern):
+        return "high"
 
 "a"
 def getRValueType(string,identation,vpatern):
     str = string.strip()
 
-    for entryType in vpatern.entry_points:
+    for entryType in vpatern.entryPoints:
         if str.startswith(entryType):
             return PHPentry(str,identation,vpatern)
-    for sanitize_type in vpatern.sanitization_functions:
+    for sanitize_type in vpatern.sanitizationFunctions:
         if str.startswith(sanitize_type):
             return Sanitization(str,identation,vpatern)
     if str.startswith('\"') & str.endswith('\";'):
         return PhpStrings(str,identation,vpatern)
-    for sinkType in vpatern.sensitive_sinks:
+    for sinkType in vpatern.sensitiveSinks:
         if re.search(sinkType, str) != None:
             return Sink(str,identation,vpatern)
     if str.startswith("$"):
         return PHPvar(str,identation,vpatern)
+    return UnknownRValue(str, identation, vpatern)
 
 def getEntrysInSink(string,identation,vpatern):
     vars = []
@@ -325,11 +251,9 @@ def getEntrysInSink(string,identation,vpatern):
     groups = re.findall("\s*(\$[a-z]\w*)\s*", string)
     for entry in groups:
         found = False
-        for entryType in vpatern.entry_points:
+        for entryType in vpatern.entryPoints:
             if entry.startswith(entryType):
                 vars.append(PHPentry(entry, identation, vpatern))
                 found = True
         if not found:
             vars.append(PHPvar(cut, identation + 1, vpatern))
-
-"xxx".strip(" ")
