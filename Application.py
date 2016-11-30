1cf2628e51322bcdc907eca7f7f840157fb8e011
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-11-30 19:50:56 +0000

    Using right application start

--- Application.py
+++ Application.py
@@ -3,18 +3,7 @@ from VulnerabilityPattern import *
 from OutputColors import *
 import sys
 
-# TODO: Remove after debugging
-path = "./Slices/"
 
-files = ["sqli_01.txt","sqli_02.txt","sqli_03.txt","sqli_04.txt","sqli_05.txt","xss_01.txt","xss_02.txt","xss_03.txt",
-         "sqli_01_sanitized.txt", "sqli_02_sanitized.txt", "sqli_03_sanitized.txt", "sqli_04_sanitized.txt",
-         "sqli_05_sanitized.txt", "xss_01_sanitized.txt", "xss_02_sanitized.txt", "xss_03_sanitized.txt",
-         "sqli_01_entry.txt", "sqli_website.txt", "teste.txt","sqli_unknown_rvalue.txt"]
-
-
-
-
-"""
 if len(sys.argv) == 1:
     print(colors.RED+"Please give the name of the slice file as an argument."+colors.RESET)
 
@@ -37,33 +26,43 @@ else:
         for pattern in patterns:
             slices.append(Slice(filename_slices, content, pattern))
 
-        for slice in slices:
-            if slice.isVulnerable():
-                print(slice.name + " | this slice is vulnerable: ")
-                slice.printVulnerabilities()
-                print("\n")
+        prev_slice_name = ""
+
+        if not debugging:
+            for slice in slices:
+                if prev_slice_name != slice.name:
+                    prev_slice_name = slice.name
+                    print("\n\n")
+                    print(colors.BLUE + "#" * 83 + colors.RESET)
+                    print(colors.BLUE + "#" * 2 + colors.RESET + slice.name.center(79,
+                                                                                   ' ') + colors.BLUE + "#" * 2 + colors.RESET)
+                    print(colors.BLUE + "#" * 83 + colors.RESET)
+
+                print("#" * 83)
+                print("#" * 2 + slice.vp.vulnerabilityName.center(79, ' ') + "#" * 2)
+                print("#" * 83)
+                if slice.isVulnerable():
+                    slice.printVulnerabilities()
+                slice.printAllVulnInfo()
+                print("\n" + colors.YELLOW + "#" * 30 + "Integrity Flow".center(23, ' ') + "#" * 30 + colors.RESET)
+                print(getGraphCaption())
+                tree = slice.getVulnTreeInfo()
+                for treeline in tree:
+                    print(treeline)
+                print("\n\n")
 
     except IOError:
         print("There was an error while opening the file. Please try again.")
+
+
+# TODO: Remove after debugging
 """
-"""
-slices = []
-for file in files:
+path = "./Slices/"
 
-    # TODO: Remove on final version
-    print("\n" + "\n" + "#" * 63 + "\n" + "#" * 23 + file + "#" * 23 + "\n" + "#" * 63 + "\n")
-
-    for pattern in patterns:
-        slices.append(Slice(path + file, pattern))
-
-# apenas diz o nome do ficheiro e qual a vulnerabilidade
-for slice in slices:
-    if slice.isVulnerable():
-        print slice.name + " | this slice is vulnerable: "
-        slice.printVulnerabilities() #TODO fazer print dentro da funcao ou fora ?
-        print "\n"
-"""
-
+files = ["sqli_01.txt","sqli_02.txt","sqli_03.txt","sqli_04.txt","sqli_05.txt","xss_01.txt","xss_02.txt","xss_03.txt",
+         "sqli_01_sanitized.txt", "sqli_02_sanitized.txt", "sqli_03_sanitized.txt", "sqli_04_sanitized.txt",
+         "sqli_05_sanitized.txt", "xss_01_sanitized.txt", "xss_02_sanitized.txt", "xss_03_sanitized.txt",
+         "sqli_01_entry.txt", "sqli_website.txt", "teste.txt","sqli_unknown_rvalue.txt"]
 
 #filename_patterns = "PatternsFile.txt"
 filename_patterns = "PatternsTest.txt"
@@ -114,3 +113,6 @@ try:
 
 except IOError:
     print("There was an error while opening the file. Please try again.")
+"""
+
+
